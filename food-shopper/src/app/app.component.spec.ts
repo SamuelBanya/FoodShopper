9219012c5c112d5e3115b0657c81afc49ac8b685
Samuel Banya <sbanya@fastmail.com>
2023-02-08 09:03:53 -0600

    Correcting name of project, and adding test h2 tag

--- food-shopper/src/app/app.component.spec.ts
+++ food-shopper/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'food-saver'`, () => {
+  it(`should have as title 'food-shopper'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('food-saver');
+    expect(app.title).toEqual('food-shopper');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('food-saver app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('food-shopper app is running!');
   });
 });
